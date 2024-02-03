d2dc3dcd2f5bd98c48b3a47a2fe99206fce44804
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-03 20:26:57 -0300

    Create Aula02.py

---
+++ Basic/Aula01/Aula02.py
@@ -0,0 +1,25 @@
+print(type(2.2))
+print(type(17))
+print(type("Hello"))
+print(type(True))
+print(type("""aaaaa"""))
+print(type('''bbbb'''))
+print('''"Oh no", she exclaimed, "Ben's bike is broken!" ''')
+print(42,500)
+print(42.500)
+
+print("============================================")
+
+print(3.14, int(3.14))
+print(3.9,int(3.9))
+print(-3.9,int(-3.9))
+print(123,int("123"))
+print(str(17))
+print(type(str(17)))
+print(str(17.18))
+print(type(str(17.18)))
+val = 50 + 5
+print("The value is "+str(val))
+
+
+
