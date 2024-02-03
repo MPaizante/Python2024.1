ebdd7dc273da10596307bdc48d26fdbf2f2eaac0
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-03 20:00:09 -0300

    Update Aula01.py

--- Basic/Aula01/Aula01.py
+++ Basic/Aula01/Aula01.py
@@ -1,2 +1,15 @@
-import math;
-print(sum(5,6))
+def square(x):
+    return x*x;
+
+def sub(x,y):
+    return x-y;
+
+print(square(3))
+print(square(5))
+print(sub(6,4))
+print(sub(5,9))
+print("=========================")
+print(square(3)+2);
+print(sub(square(3),square(1+1)))
+
+print(square)
