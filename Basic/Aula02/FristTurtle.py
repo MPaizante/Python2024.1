c440a3d255b27d3389c96d6af8ea60dd165362ae
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-04 00:02:48 -0300

    Update FristTurtle.py

--- Basic/Aula02/FristTurtle.py
+++ Basic/Aula02/FristTurtle.py
@@ -4,3 +4,20 @@ alex = turtle.Turtle()
 alex.forward(150)
 alex.left(90)
 alex.forward(75)
+alex.left(90)
+alex.forward(150)
+alex.left(90)
+
+print("=====================")
+wz = turtle.Screen()
+wz.bgcolor("lightgreen")       
+
+tess = turtle.Turtle()
+tess.color("blue")              
+tess.pensize(3)                 
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.exitonclick()  
