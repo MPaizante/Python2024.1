8f3a6bae42ca094e3efc766ddfd52f946c4d1e1d
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-05 15:10:15 -0300

    Create Aula04-1.py

---
+++ Basic/Aula02/Aula04-1.py
@@ -0,0 +1,6 @@
+pi = float(3.14159)
+thunder = input("Enter a number: ")
+thunder = float(thunder)
+area = thunder*thunder*pi
+print("A area é: " + str(round(area,2)))
+print("A area é: {}".format(area))
