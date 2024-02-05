86044cb71d13a0f8870bf5b4d5a52aa0373629cd
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-05 15:16:50 -0300

    Create Aula04-2.py

---
+++ Basic/Aula02/Aula04-2.py
@@ -0,0 +1,7 @@
+n1 = input(("Enter the first note: "))
+n2 = input(("Enter the second note: "))
+n3 = input(("Enter the third note: "))
+n1 = float(n1)*0.2
+n2 = float(n2)*0.3
+n3 = float(n3)*0.5
+print("A media do aluno foi: "+ str(n1+n2+n3))
