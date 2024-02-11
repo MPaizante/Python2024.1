861a1656dea52af24fb8f6dbac99c2361ea84b88
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-10 21:36:06 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,19 +2,26 @@
  #pip --pre install Pillow 
 
 
+import turtle
+
+# Set up the turtle
+t = turtle.Turtle()
+turtle.bgcolor("black")
+t.speed(0)  # Set the turtle's speed to draw super fast with no animation
+
+# Define colors
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+# Draw a colorful spiral
+for _ in range(200):
+    t.pencolor(colors[_ % 7])  # Cycle through colors
+    t.forward(_)
+    t.left(51)
+
+# Hide the turtle
+t.hideturtle()
+
+# Keep the window open
+turtle.done()
 
 
-
-
-addition_str = "2+5+10+20"
-sum_val = 0
-addition_str = addition_str.split("+")
-y = int(addition_str)
-
-print(addition_str)
-for x in y:
-    print(x)
-
-
-
-print(sum_val)
