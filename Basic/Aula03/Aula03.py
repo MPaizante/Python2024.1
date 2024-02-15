65dc9b534ef53edee371b4224814a1cc7f44263f
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-14 21:18:14 -0300

    Create Aula03.py

---
+++ Basic/Aula03/Aula03.py
@@ -0,0 +1,20 @@
+ddd = int(input("Informe o DDD: "))
+
+if ddd == 21:
+    print("RJ")
+elif ddd == 61:
+    print("DF")
+elif ddd == 71:
+    print("BA")
+elif ddd == 11:
+    print("SP")
+elif ddd == 32:
+    print("JF")
+elif ddd == 19:
+    print("CP")
+elif ddd == 27:
+    print("VT")
+elif ddd == 31:
+    print("MG")
+else:
+    print("Digitou DDD errado!")
