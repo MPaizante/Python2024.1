6ad5842969ba423ce1a3a0e7b20b3878e79a1380
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-14 23:22:24 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,6 @@
-s = "singing in the rain and playing in the rain are two entirely different situations but both can be fun"
+x = int(input("Digite:"))
 
-num_vowels = 0
-a = s.split()
-vowels = ['a','e','i','o','u']
-
-for x in s:
-    if x in vowels:
-        num_vowels +=1
-
-print(num_vowels)
-
+for y in range(x+1):
+    if (0<y):
+        if(y % 2 == 0):
+            print(y*y)
