98f077541c2c074b5d8cba1c5e35a4f13af1c6e8
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-18 19:30:49 -0300

    ;

--- Test.py
+++ Test.py
@@ -1,8 +1,2 @@
-origlist = [45,32,88]
-aliaslist = origlist
-origlist += ["cat"]
-origlist = origlist + ["cow"]
-
-print(origlist is aliaslist)
-print(origlist, aliaslist)
-
+s = "python rocks"
+print(s[1]*s.index("n"))
