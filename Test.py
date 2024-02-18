5102793a05ec100d71c3de78fe4885e1c011ed12
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-18 19:05:21 -0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,29 +1,8 @@
-def ler_numeros():
-    numeros = []
-    while True:
-        linha = input("Digite um número (ou pressione Enter para encerrar): ")
-        if not linha:
-            break  # Encerra o loop quando uma linha vazia for digitada
-        try:
-            numero = float(linha)
-            numeros.append(numero)
-        except ValueError:
-            print("Por favor, digite um número válido.")
+origlist = [45,32,88]
+aliaslist = origlist
+origlist += ["cat"]
+origlist = origlist + ["cow"]
 
-    return numeros
+print(origlist is aliaslist)
+print(origlist, aliaslist)
 
-def numeros_maiores_que_ultimo(numeros):
-    if not numeros:
-        print("Nenhum número foi inserido.")
-        return
-
-    ultimo_numero = numeros[-1]
-    numeros_maiores = [num for num in numeros if num > ultimo_numero]
-
-    print("Números maiores que o último número lido:")
-    for numero in numeros_maiores:
-        print(numero)
-
-# Programa principal
-numeros = ler_numeros()
-numeros_maiores_que_ultimo(numeros)
