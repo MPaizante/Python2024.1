f414ee7091c3653c24a9be40b35f5cc007157e2e
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-23 21:09:19 -0300

    Update Ex7.py

---
+++ Basic/Aula06/Ex7.py
@@ -0,0 +1,21 @@
+sentence = "The dog chased the rabbit into the forest but the rabbit was too quick."
+x = sentence.split()
+word_counts = {}
+for i in x:
+    if i not in word_counts:
+        word_counts[i] = 0
+    word_counts[i] += 1
+print(word_counts)
+
+
+
+stri = "what can I do"
+char_d = {}
+x = stri.split()
+for i in x:
+    for u in i:
+        if u not in char_d:
+            char_d[u] = 0
+        char_d[u] += 1
+        
+print(char_d)
