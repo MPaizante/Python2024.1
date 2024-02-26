8066c68d2bdeb959450fd5dff2a61b3ac62be293
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-26 19:32:33 -0300

    Create Aula06.py

---
+++ Basic/Aula07/Aula06.py
@@ -0,0 +1,32 @@
+def int_return(x):
+    return x
+
+def add(x):
+    return x+2
+
+
+def change(x):
+    return x + 'Nice to meet you!'
+
+def accum(x):
+    c = 0
+    for i in x:
+        c += i
+
+    return c
+
+
+def length(x):
+    if len(x) >= 5:
+        return "Longer than 5"
+    else:
+        return "Less than 5"
+
+
+
+def divide(x):
+    return x/2
+
+def sum(y):
+    return (y/2)+6
+
