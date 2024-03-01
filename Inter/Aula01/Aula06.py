2ffdab31e0e530fdb67e265bbf9204a28da80ffe
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-01 19:47:43 -0300

    ;

---
+++ Inter/Aula01/Aula06.py
@@ -0,0 +1,49 @@
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_version = original[:]
+print(copied_version)
+print(copied_version is original)
+print(copied_version == original)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_version)
+print('-'*30)
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = []
+    for item in inner_list:
+        copied_inner_list.append(item)
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
+print('-'*30)
+original = [['dogs', 'puppies'], ['cats', "kittens"]]
+copied_outer_list = []
+for inner_list in original:
+    copied_inner_list = inner_list[:]
+    copied_outer_list.append(copied_inner_list)
+print(copied_outer_list)
+original[0].append(["canines"])
+print(original)
+print("-------- Now look at the copied version -----------")
+print(copied_outer_list)
+
+print('-'*30)
+import copy
+original = [['canines', ['dogs', 'puppies']], ['felines', ['cats', 'kittens']]]
+shallow_copy_version = original[:]
+deeply_copied_version = copy.deepcopy(original)
+original.append("Hi there")
+original[0].append(["marsupials"])
+print("-------- Original -----------")
+print(original)
+print("-------- deep copy -----------")
+print(deeply_copied_version)
+print("-------- shallow copy -----------")
+print(shallow_copy_version)
+
+
