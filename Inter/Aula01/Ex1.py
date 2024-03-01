950ec3167b93aa832a8a4a0a901de974f5dc0ebd
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-01 20:33:06 -0300

    ;

---
+++ Inter/Aula01/Ex1.py
@@ -0,0 +1,20 @@
+nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
+output = nested[1][2]
+print(output)
+
+lst = [['apple', 'orange', 'banana'], [5, 6, 7, 8, 9.9, 10], ['green', 'yellow', 'purple', 'red']]
+if 'yellow' in lst[2]:
+    yellow = True
+else:
+    yellow = False
+print(yellow)
+if 4 in lst[1]:
+    four = True
+else:
+    four = False
+print(four)
+if 'orange' in lst[0]:
+    orange = True
+else:
+    orange =False
+print(orange)
