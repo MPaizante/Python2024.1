872504369f037d6452706ded85d8f4bdc9c9024d
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-29 21:15:12 -0300

    Update Aula02.py

---
+++ Basic/Aula10/Aula02.py
@@ -0,0 +1,26 @@
+L = ['E', 'F', 'B', 'A', 'D', 'I', 'I', 'C', 'B', 'A', 'D', 'D', 'E', 'D']
+
+d = {}
+for x in L:
+    if x in d:
+        d[x] = d[x] + 1
+    else:
+        d[x] = 1
+y = sorted(d.keys())
+for k in y:
+    print("{} appears {} times".format(k, d[k]))
+
+
+print('-'*30)
+L = ['E', 'F', 'B', 'A', 'D', 'I', 'I', 'C', 'B', 'A', 'D', 'D', 'E', 'D']
+
+d = {}
+for x in L:
+    if x in d:
+        d[x] = d[x] + 1
+    else:
+        d[x] = 1
+
+y = sorted(d.keys(), key=lambda k: d[k], reverse=True)
+for k in y:
+    print("{} appears {} times".format(k, d[k]))
