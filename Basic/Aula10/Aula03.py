032be4a9f52215490514c72c6f632c50a5b08170
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-02-29 21:40:39 -0300

    Update Aula03.py

---
+++ Basic/Aula10/Aula03.py
@@ -0,0 +1,11 @@
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+
+sorted_keys = sorted(dictionary.keys())
+print(sorted_keys)
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+grocery_keys_sorted = sorted(groceries.keys())
+
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+sorted_values = sorted(dictionary.keys(),key= lambda x:dictionary[x],reverse=True)
+print(sorted_values)
