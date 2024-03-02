8f60fdee77a220e28809c1f08e762c27fe4adecc
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-01 23:06:05 -0300

    Update Aula01.py

---
+++ Inter/Aula02/Aula01.py
@@ -0,0 +1,39 @@
+def doubleStuff(a_list):
+    """ Return a new list in which contains doubles of the elements in a_list. """
+    new_list = []
+    for value in a_list:
+        new_elem = 2 * value
+        new_list.append(new_elem)
+    return new_list
+
+things = [2, 5, 9]
+print(things)
+things = doubleStuff(things)
+print(things)
+
+
+def triple(value):
+    return 3*value
+
+def tripleStuff(a_list):
+    new_seq = map(triple, a_list)
+    return list(new_seq)
+
+def quadrupleStuff(a_list):
+    new_seq = map(lambda value: 4*value, a_list)
+    return list(new_seq)
+
+things = [2, 5, 9]
+things3 = tripleStuff(things)
+print(things3)
+things4 = quadrupleStuff(things)
+print(things4)
+
+
+things = [2, 5, 9]
+
+things4 = map((lambda value: 4*value), things)
+print(list(things4))
+
+# or all on one line
+print(list(map((lambda value: 5*value), [1, 2, 3])))
