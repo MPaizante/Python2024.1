e056fc68deebdf5affffb2797fca0aa4f9585fac
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-17 15:38:28 -0300

    .

---
+++ Inter/Aula04Classes/Aula2.py
@@ -0,0 +1,22 @@
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def getX(self):
+        return self.x
+
+
+Point1 = Point(5,10)
+Point2 = Point(1,2)
+print(Point1.getX())
+
+class P():
+    def __init__(self,x , y):
+        self.x = x
+        self.y = y
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+
+po = P(10,100)
---
+++ Inter/Aula04Classes/Ex1.py
@@ -0,0 +1,9 @@
+class NumberSet():
+    def __init__(self, num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+
+
+Point = NumberSet(6,10)
+
+print(Point.num1,Point.num2)
