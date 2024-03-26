4ac784b8d1935e071ddb5acb47cb2c0a9ed05bfb
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-25 21:17:16 -0300

    ;

--- Inter/Aula10Test/Aula01.py
+++ Inter/Aula10Test/Aula01.py
@@ -1,20 +1,28 @@
-entrada = input('Digite o vogal: ').upper()
-lista = []
-while True:
-    linha = input('Digite a sua frase: ').upper().split()
-    if not linha:
-        break
-    lista.append(linha)
-c = 0
-lista2=[]
-for i in lista:
-    for u in i:
-        if u[0] == entrada or u[-1] == entrada:
-            c +=1
-            lista2.append(u)
-for y in lista2:
-    print(y)
-print(f'Iniciadas ou Finalizadas pelo Caracter {entrada} = {c} Vez(es)')
-
-
-
+def lista(x):
+    lista = []
+    for i in range(x):
+        numeros = int(input('Numero: '))
+        lista.append(numeros)
+    return lista
+def menor(lista):
+    menor = lista[0]
+    for i in lista:
+        if i < menor:
+            menor = i
+    return menor
+def maior(lista):
+    maior = lista[0]
+    for i in lista:
+        if i > maior:
+            maior = i
+    return maior
+def media(lista):
+    c = 0
+    for i in lista:
+        c += i
+    return c
+entrada = int(input('Quantos numeros quer ler?: '))
+valor = lista(entrada)
+print(f'Menor dos {entrada} numeros lidos foi: {menor(valor)}')
+print(f'Maior dos {entrada} numeros lidos foi: {maior(valor)}')
+print(f'A media dos {entrada} numeros foi {(media(valor)/len(valor)):,.2f}')
