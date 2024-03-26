0f1f1b18e081660184bcc0ee81c62a794f3550b4
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-03-26 20:28:00 -0300

    Update Aula01.py

--- Inter/Aula10Test/Aula01.py
+++ Inter/Aula10Test/Aula01.py
@@ -1,28 +1,23 @@
-def lista(x):
+def linha():
     lista = []
-    for i in range(x):
-        numeros = int(input('Numero: '))
-        lista.append(numeros)
+    while True:
+        linha = input('Digite: ')
+        if not linha:
+            break
+        lista.append(linha)
+        lista = [float(num) for num in lista]
     return lista
-def menor(lista):
-    menor = lista[0]
-    for i in lista:
-        if i < menor:
-            menor = i
-    return menor
 def maior(lista):
-    maior = lista[0]
+    maior = lista[-1]
+    lista_maior = []
     for i in lista:
         if i > maior:
-            maior = i
-    return maior
-def media(lista):
-    c = 0
-    for i in lista:
-        c += i
-    return c
-entrada = int(input('Quantos numeros quer ler?: '))
-valor = lista(entrada)
-print(f'Menor dos {entrada} numeros lidos foi: {menor(valor)}')
-print(f'Maior dos {entrada} numeros lidos foi: {maior(valor)}')
-print(f'A media dos {entrada} numeros foi {(media(valor)/len(valor)):,.2f}')
+            lista_maior.append(i)
+    return lista_maior
+
+
+numeros = linha()
+maiores = maior(numeros)
+print(f'Relação dos numeros maiores que {numeros[-1]}: ')
+for i in maiores:
+    print(i)
