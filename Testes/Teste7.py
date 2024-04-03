2885b988d80e923d6e137a683229d673e4638d82
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-04-03 20:56:15 -0300

    Create Teste7.py

---
+++ Testes/Teste7.py
@@ -0,0 +1,19 @@
+def mostrar(vts):
+    listagemVazia = True
+    for i in range(len(vts)):
+        if vts[i] != 0:
+            listagemVazia = False
+            print("Candidato", i, "obteve", vts[i], "voto(s)")
+    if listagemVazia:
+        print( "Nenhum voto ocorreu!!!")
+    return None
+
+
+# Programa Principal
+votos = [0]*10
+linha = input()
+while linha != "":
+    votos[int(linha)] += 1
+    linha = input()
+mostrar(votos)
+
