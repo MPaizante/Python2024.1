da55e236c4e401086e4c3c90556bba9dc085f9ad
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-04-03 21:53:51 -0300

    Update Teste7.py

--- Testes/Teste7.py
+++ Testes/Teste7.py
@@ -1,19 +1,11 @@
-def mostrar(vts):
-    listagemVazia = True
-    for i in range(len(vts)):
-        if vts[i] != 0:
-            listagemVazia = False
-            print("Candidato", i, "obteve", vts[i], "voto(s)")
-    if listagemVazia:
-        print( "Nenhum voto ocorreu!!!")
-    return None
+lista = []
+entrada = input()
+if not entrada:
+    print("Nenhum voto ocorreu!")
+while (entrada != ""):
+    lista.append(entrada)
+    entrada = input()
 
-
-# Programa Principal
 votos = [0]*10
-linha = input()
-while linha != "":
-    votos[int(linha)] += 1
-    linha = input()
-mostrar(votos)
+
 
