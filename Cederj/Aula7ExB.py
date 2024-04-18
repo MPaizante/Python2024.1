1572a5033232bcd5858730971ec65a5da5abbab1
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-04-17 21:37:53 -0300

    Update Aula7ExB.py

--- Cederj/Aula7ExB.py
+++ Cederj/Aula7ExB.py
@@ -1,3 +1,17 @@
 lista = []
+x = True
 while True:
-    n = int(input("Digite"))
+    n = input("Digite: ")
+    if not n:
+        break
+    lista.append(n)
+lista = [int(num) for num in lista]
+
+maior = 0
+pos = 0
+for i in lista:
+    if i > maior:
+        maior = i
+        pos = lista.index(i)
+print(f'Maior numero lido foi {maior} na posição {pos+1}')
+
