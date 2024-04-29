c34418108c308209823cf783c9ab66f053433a0d
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-04-28 22:19:10 -0300

    Create Aula05.py

---
+++ Cederj/Diretorios/Aula01/Aula05.py
@@ -0,0 +1,23 @@
+Junior = {'SI 206':4, 'SI 310':4, 'BL 300':3, 'TO 313':3, 'BCOM 350':1, 'MO 300':3}
+credits = 0
+for i in Junior:
+    credits += Junior[i]
+print(credits)
+
+str1 = "peter piper picked a peck of pickled peppers"
+freq = {}
+for i in str1:
+    if i not in freq:
+        freq[i] = 1
+    else:
+        freq[i] += 1
+print(freq)
+
+s1 = "hello"
+counts = {}
+for i in s1:
+    if i not in counts:
+        counts[i] = 1
+    else:
+        counts[i] += 1
+print(counts)
