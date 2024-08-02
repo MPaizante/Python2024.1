5e84146ffff0566420a235f7d083ec9d8fa50ce4
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-08-02 19:11:31 -0300

    Update Aula3.py

--- Interfaces/Aula3.py
+++ Interfaces/Aula3.py
@@ -13,13 +13,21 @@ class Point:
         res = ((self.x)**2+(self.y))**0.5
         return res
     def __str__(self):
-        return f'Ponit: ({self.x},{self.y}) =  '
+        return f'Ponit: ({self.x},{self.y}) = {((self.x)**2+(self.y))**0.5} '
+    def halfwary(self, target):
+        mx = (self.x + target.x)/2
+        my = (self.y + target.y)/2
+        return Point(mx,my)
     
-valores = input("Digite o ponto x e y: ").split()
-valores_int = [int(n) for n in valores]
-rs = Point(valores_int[0],valores_int[1])
+#valores = input("Digite o ponto x e y: ").split()
+#valores_int = [int(n) for n in valores]
+p1 = Point(3,4)
+p2 = Point(5,12)
+mid = p1.halfwary(p2)
+print(f"Media pontos x e y: ",mid.getX(),mid.getY())
 
-print(rs)
+
+
 
 class Cereal:
     def __init__(self,name,brand,fiber):
@@ -30,6 +38,7 @@ class Cereal:
     def __str__(self):
         return f"{self.name} cereal is produced by {self.brand} and has {self.fiber} grams of fiber in every serving!"
 
+    
 c1 = Cereal("Corn Flakes","Kellogg's",2)   
 c2 = Cereal("Honey Nut Cheerios","General Mills",3 ) 
 print(c1)
