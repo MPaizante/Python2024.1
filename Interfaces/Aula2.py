81baa470b0a7ad1e78c9b4fb4aa926bd304688b0
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-08-01 22:08:36 -0300

    Update Aula2.py

--- Interfaces/Aula2.py
+++ Interfaces/Aula2.py
@@ -11,3 +11,18 @@ class Point():
 point1 = Point(7,6)
 print(f'Os pontos são ({point1.getX()},{point1.getY()}) a distancia da origem é {point1.distanceFromOrigin():,.1f}')
 
+
+
+class Animal(object):
+    def __init__(self, arms,legs):
+        self.arms = arms
+        self.legs = legs
+    def limbs(self):
+        return self.arms + self.legs
+
+spider = Animal(4,4)
+spidlimbs = spider.limbs()
+print(spidlimbs)
+        
+    
+
